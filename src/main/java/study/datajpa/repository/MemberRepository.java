9d4bdea798a265b66d78451c3ea091fc08e16023
YDH <ydhwa_18@naver.com>
2020-01-13 22:24:14 +0900

    Query method

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,11 +1,21 @@
 package study.datajpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import study.datajpa.entity.Member;
 
 import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    List<Member> findByUsername(String username);   // query method
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);   // query method
+
+    List<Member> findTop3HelloBy();
+
+//    @Query(name = "Member.findByUsername")
+    List<Member> findByUsername(@Param("username") String username);
+
+    @Query("select m from Member m where m.username = :username and m.age = :age")
+    List<Member> findMember(@Param("username") String username, @Param("age") int age);
 }
