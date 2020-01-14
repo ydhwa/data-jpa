fa03e5b5b9434691fd72ae41fc68fcc8b3bfc2bd
YDH <ydhwa_18@naver.com>
2020-01-14 23:27:37 +0900

    Auditing

--- src/main/java/study/datajpa/DataJpaApplication.java
+++ src/main/java/study/datajpa/DataJpaApplication.java
@@ -2,8 +2,15 @@ package study.datajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import java.util.Optional;
+import java.util.UUID;
+
+@EnableJpaAuditing
 @SpringBootApplication
 //@EnableJpaRepositories(basePackages = "study.datajpa.repository")
 public class DataJpaApplication {
@@ -12,4 +19,9 @@ public class DataJpaApplication {
 		SpringApplication.run(DataJpaApplication.class, args);
 	}
 
+	@Bean
+	public AuditorAware<String> auditorProvider() {
+		// @Autowired Optional<String> getCurrentAuditor()
+		return () -> Optional.of(UUID.randomUUID().toString());
+	}
 }
